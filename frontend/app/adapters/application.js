50e45ea0ebaea1d94ce10978b3fd6d3ac0e320cb
Alvaro Uribe <alvaro.uribe@vocus.co.nz>
2016-07-21 14:37:01 +1200

    Fixing model in index route

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,5 +1,6 @@
 // import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import RESTAdapter from 'ember-data/adapters/rest';
+
 // export default JSONAPIAdapter.extend({
 export default RESTAdapter.extend({
   host: 'http://localhost:3000',
--- server.js
+++ server.js
@@ -3,14 +3,20 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var morgan = require('morgan'); // HTTP request logger middleware for node.js
+// var methodOverride = require('method-override'); // Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it.
 
 // MongoDB
 mongoose.connect('mongodb://localhost/rest_test');
 
 // Express
 var app = express();
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// app.use(bodyParser.json({type:'application/vnd.api+json'}));
+// app.use(methodOverride());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
